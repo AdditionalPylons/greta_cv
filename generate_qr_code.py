804915aeb46f734c22add8792831f6c82c9b23cc
Bill <bill@popstack.io>
2024-10-26 15:25:36 -0600

    Correct URL

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image
 
 qr = qrcode.QRCode(box_size=5)
-qr.add_data("www.bytesitesolutions.com")
+qr.add_data("https://github.com/AdditionalPylons/greta_cv/blob/main/Greta%20Leroux%20Biomagnetismo.pdf?raw=true")
 qr.make(fit=True)
 img = qr.make_image(fill_color='white', back_color='black')
 img.show()
